cc7f2a3b84f8052a656e4e32cb9208ec48c60393
Zimon26 <31433044@qq.com>
2022-12-03 21:05:58 +0800

    big progress next add username setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
 import { Button, Input, message } from 'antd'
 import 'antd/dist/reset.css'
-import { useRef, useState } from 'react'
+import axios from 'axios'
+import { useEffect, useRef, useState } from 'react'
 import './App.less'
 
 function App() {
   const [username, setUsername] = useState('Zimon')
   const [collaborators, setCollaborators] = useState(['Zimon, Luo'])
   const [isWorking, setIsWorking] = useState(false)
+  const [thisArrayID, setThisArrayID] = useState(0)
   const [messageApi, contextHolder] = message.useMessage()
   const joinFormRef = useRef()
   const textareaRef = useRef()
-  const text = []
+  const joinColInputRef = useRef()
+  let text = []
   let startPos, endPos
 
-  // useEffect(() => {
-  //   // 初始化做的事
-  //   // setInterval(() => {
-  //   //   console.log(textareaRef.current.selectionStart)
-  //   //   console.log(textareaRef.current.selectionEnd)
-  //   // }, 1000)
-  // }, [])
+  useEffect(() => {
+    // 初始化做的事
+    // setInterval(() => {
+    //   text.push('m')
+    // }, 1000)
+    // async function initID() {
+    //   const { data: res } = await axios.get('http://localhost:8888/init')
+    //   console.log(res)
+    //   thisID = res
+    // }
+    // initID()
+    return () => { }
+  }, [])
 
-  const launchCol = (e) => {
+  const launchCol = async (e) => {
     setIsWorking(true)
     //  请求后端获得协作码
-    const code = '02026'
+    const { data: { code, collaborators, arrayID } } = await axios.get('http://localhost:8888/launch-col', {
+      params: {
+        username: username
+      }
+    })
+    setCollaborators(collaborators)
+    setThisArrayID(arrayID)
+    console.log(arrayID)
     messageApi.info(`您的协作码是 ${code}`)
-    e.target.innerHTML = '协作中 02026'
+    e.target.innerHTML = `协作中 ${code}`
     joinFormRef.current.style.display = 'none'
-    // 发送消息提示
-    // 通知后端
   }
 
   const joinCol = () => {
@@ -44,50 +58,84 @@ function App() {
     joinFormRef.current.style.display = 'none'
   }
 
-  const submitCode = () => {
+  const submitCode = async () => {
     // 发送后端比对
-    console.log('click')
+    const code = joinColInputRef.current.input.value
+    if (code.length === 5) {
+      const { data: { number, collaborators, text, arrayID } } = await axios.get('http://localhost:8888/join-col', {
+        params: {
+          code: code,
+          username: username
+        }
+      })
+      if (number === 1) {
+        setCollaborators(collaborators)
+        setThisArrayID(arrayID)
+        textareaRef.current.value = text.join('')
+        joinFormRef.current.style.display = 'none'
+        messageApi.info('您已成功加入协作')
+      } else if (number === 0) {
+        messageApi.info('此协作码没有对应协作，请重新输入或者发起协作')
+      }
+    } else {
+      messageApi.info('协作码为五位数字，请重新输入')
+    }
+
+
   }
 
-  const handleInput = (e) => {
+  const handleInput = async (e) => {
     console.log(e)
+    // console.log(textareaRef.current)
     const data = e.nativeEvent.data
     // const start = textareaRef.current.selectionStart
     // const end = textareaRef.current.selectionEnd
     const start = startPos
     const end = endPos
+    let args = []
     switch (e.nativeEvent.inputType) {
       case 'insertText':
-        text.splice(start + 1, 0, data)
+        args = [start + 1, 0, data]
+        // text.splice(start + 1, 0, data)
         // console.log(text.join(''))
         break
       case 'deleteContentBackward':
-        console.log(start, startPos)
-        console.log(end, endPos)
+        // console.log(start, startPos)
+        // console.log(end, endPos)
         if (start === end) {
-          text.splice(start - 1, 1)
+          args = [start - 1, 1]
+          // text.splice(start - 1, 1)
         } else if (end > start) {
-          text.splice(start, end - start)
+          args = [start, end - start]
+          // text.splice(start, end - start)
         }
         // console.log(text.join(''))
         break
       case 'deleteContentForward':
-        console.log(start)
-        text.splice(start, 1)
+        args = [start, 1]
+        // text.splice(start, 1)
         break
 
     }
+    const { data: res } = await axios.get('http://localhost:8888/handle-change', {
+      params: {
+        arrayID: thisArrayID,
+        args: args
+      }
+    })
+    text = res
     console.log(text.join(''))
+    textareaRef.current.value = text.join('')
   }
 
-  const trackKey = (e) => {
+  const trackKey = () => {
     startPos = textareaRef.current.selectionStart
     endPos = textareaRef.current.selectionEnd
     console.log(startPos)
     console.log(endPos)
   }
 
-  const trackMouse = (e) => {
+  const trackMouse = () => {
     startPos = textareaRef.current.selectionStart
     endPos = textareaRef.current.selectionEnd
     console.log(startPos)
@@ -132,14 +180,15 @@ function App() {
           </ul>
         </div>
         <div className="main-container">
-          <textarea ref={textareaRef}
+          <textarea
+            ref={textareaRef}
             onInput={handleInput}
             onKeyDown={trackKey}
             onMouseUp={trackMouse}
             onPaste={trackPaste}
           ></textarea>
           <div className="join-form" ref={joinFormRef}>
-            <Input placeholder="请输入协作码" />
+            <Input ref={joinColInputRef} placeholder="请输入协作码" />
             <Button onClick={submitCode}>提交</Button>
           </div>
           {/* <div className="dev-show" style={{ position: 'absolute', top: '20px', right: '20px' }}>
